9838748b5142f99a16589b5cf883d1feeb8c7235
kamaboko1 <github65@hotmail.com>
2019-04-22 13:41:59 -0700

    full project commit for social-card

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div>
+            <img className="card--banner" src=" https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt="card banner"/>
+        </div>
+    )
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="card--content">
+            <p className="card--content-title">Get started with React</p>
+            <p className="card--content-description">
+              React makes it painless to create interactive UIs.  Design simle views for each state in your application.
+            </p>
+            <p className="card--content-react">reactjs.org</p>
+        </div>
+    )
+}
+
+export default CardContent;
